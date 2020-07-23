7f0354940991864b533627b96586372c85fd04c7
akash1729 <aakash.aggarwal03@gmail.com>
2020-07-24 01:26:45 +0530

    Made the base web page. The canvas with algo.
    Need to add animation.,readme,and more obstacles.

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import BFS from './BFS/BFS2';
+
+
+function App() {
+  return (
+    <div className="App">
+     <BFS>
+
+     </BFS>
+    </div>
+  );
+}
+
+export default App;
